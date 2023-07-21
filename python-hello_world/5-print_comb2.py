3f975f8b93b9d7bc56efb5a212ca58e29953a7de
abdelkaled <abdelkhalek.harou@gmail.com>
2023-07-21 21:00:01 +0100

    updated

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,9 +1,6 @@
-#!/usr/bin/python3
 def print_numbers():
-    for i in range(100):
-        if i < 99:
-            print("{:02d}, ".format(i), end="")
-        else:
-            print("{:02d}".format(i))
+    numbers = ", ".join("{:02d}".format(i) for i in range(100))
+    print(numbers)
 
 print_numbers()
+
